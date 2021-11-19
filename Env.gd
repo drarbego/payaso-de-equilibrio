f30ac0099d6f8fffea8142efbded0749c6a13c8c
3D <drarbego@gmail.com>
2021-11-19 01:10:18 -0600

    Save scores

--- Env.gd
+++ Env.gd
@@ -5,19 +5,59 @@ const cloud_texture = preload("res://Assets/Background/Cloud_01.png")
 const GameOverMenu = preload("res://GameOverMenu.tscn")
 
 export(float) var CLOUD_ANIMATION_SPEED = 300
-var score = 0
+var hiscore = 0
+const MAX_HISCORES = 5
 
 func _ready():
 	randomize()
-	$Score/Points.set_text(str(self.score))
+	$Score/Points.set_text(str(self.hiscore))
+
+func save_score():
+	var scores = self._read_scores()
+
+	var pos = 0
+	for score in scores:
+		if score < self.hiscore:
+			break
+		pos += 1
+
+	scores.insert(pos, self.hiscore)
+
+	self._write_scores(scores.slice(0, MAX_HISCORES-1))
+
+func _read_scores():
+	var file_descriptor = File.new()
+
+	if not file_descriptor.file_exists("user://hiscores.save"):
+		return []
+
+	file_descriptor.open("user://hiscores.save", File.READ)
+
+	var scores = []
+	while file_descriptor.get_position() < file_descriptor.get_len() and len(scores) < MAX_HISCORES:
+		scores.append(int(file_descriptor.get_line()))
+	
+	file_descriptor.close()
+
+	return scores
+
+func _write_scores(scores):
+	var file_descriptor = File.new()
+	file_descriptor.open("user://hiscores.save", File.WRITE)
+
+	for score in scores:
+		file_descriptor.store_line(str(score))
+		print("saved to file ", str(score))
+
+	file_descriptor.close()
 
 func add_score():
-	self.score += 10
-	$Score/Points.set_text(str(self.score))
+	self.hiscore += 10
+	$Score/Points.set_text(str(self.hiscore))
 
 func _on_obstacle_destroyed(points):
-	self.score += points
-	$Score/Points.set_text(str(score))
+	self.hiscore += points
+	$Score/Points.set_text(str(hiscore))
 
 func _process(delta):
 	animate_clouds(delta)
@@ -81,6 +121,8 @@ func _on_Payasito_shot(dir):
 	$Projectiles.add_child(projectile)
 
 func game_over():
+	self.save_score()
+
 	get_tree().get_root().get_node("Env").queue_free()
 	var game_over_menu = GameOverMenu.instance()
 	get_tree().get_root().add_child(game_over_menu)
